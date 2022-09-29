56e70af5392d5578955b8e0c3d00237e536587b5
spawn1978 <leguizamoncarlos@gmail.com>
2022-09-29 11:02:56 -0300

    Add line const password for test Snyk

--- helpers/msg.js
+++ helpers/msg.js
@@ -1,6 +1,7 @@
 function msg(req) {
   const success = req.session['success'];
   const warning = req.session['warning'];
+  const password = "1234";
 
   req.session['success'] = null;
   req.session['warning'] = null;
